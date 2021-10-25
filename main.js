f07f500d860dd62152c48295caf0f29c8f70c03f
gilfriedman <gil.s.friedman@gmail.com>
2021-10-25 23:52:54 +0300

    promise all + generalize (prompt)

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-fetch("https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=evolution&limit=500")
-.then(resp => resp.json())
-.then(async function(data) {
-    let html = "<table><tr><td>name</td><td>url</td><td>he name</td></tr>"
-    for (let key in data[1]) {
-        let heName = await fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&titles="+ data[1][key] +"&prop=langlinks&format=json&lllang=he&lllimit=100")
-        .then(resp => resp.json()).then(function(data) {
-            let langLinks = data.query.pages[Object.keys(data.query.pages)[0]].langlinks
-            let langLink = langLinks && langLinks.find(langObj => langObj.lang == "he");
-            return langLink && langLink[Object.keys(langLink)[1]];
-        });
-        html += `<tr>
+const input = prompt();
+
+fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${input}&limit=500`)
+    .then(resp => resp.json())
+    .then(async function (data) {
+        document.write("<table><tr><td>name</td><td>url</td><td>he name</td></tr>");
+
+        const urls = data[1].map(title => `https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=${title}&prop=langlinks&format=json&lllang=he&lllimit=500`);
+        const responses = await Promise.all(urls.map(async url => {
+            const resp = await fetch(url);
+            return resp.json();
+        }));
+        const langLinks = responses.map(response => response.query.pages[Object.keys(response.query.pages)[0]].langlinks);
+        const heNames = langLinks.map(langLink => langLink && langLink[0][Object.keys(langLink[0])[1]]);
+
+        for (let key in data[1]) {
+            document.write(`<tr>
         <td>${data[1][key]}</td>
-        <td>${data[3][key]}</td>
-        <td>${heName}</td>
-        </tr>`
-    }
-    html += "</table>";
-    document.write(html);
-    console.table(data);
-});
+        <td>${data[3][key]}</td>`
+                + (heNames[key] ? `<td><a href="https://he.wikipedia.org/wiki/${heNames[key]}">${heNames[key]}</a></td>` : "<td></td>")
+                + `</tr>`);
+        }
+    });
