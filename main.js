e795984d6239b1ad44aee85426de8eb3b5740a0a
gilfriedman <gil.s.friedman@gmail.com>
2021-11-14 00:16:50 +0200

    finished pagesHaveEvolutionSection

--- main.js
+++ main.js
@@ -10,28 +10,128 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 const BASE_URL = "https://en.wikipedia.org/w/api.php";
 let input = "Category:Animals";
+/* MAIN */
 (() => __awaiter(this, void 0, void 0, function* () {
-    const hi = yield getSubcatsByCatAndLevel(input, 1);
-    debugger;
+    var e_1, _a;
+    /* START getSubcatsByCatAndLevel */
+    const hi = yield getSubcatsByCatAndLevel(input, 0);
     console.log(hi);
     const set = new Set(hi.map(x => x.title));
     console.log(set);
+    /* END getSubcatsByCatAndLevel */
+    /* START getAllPagesInCat */
+    let articles = [];
+    try {
+        //debugger
+        for (var set_1 = __asyncValues(set), set_1_1; set_1_1 = yield set_1.next(), !set_1_1.done;) {
+            const cat = set_1_1.value;
+            articles.push(...yield getAllPagesInCat(cat));
+        }
+    }
+    catch (e_1_1) { e_1 = { error: e_1_1 }; }
+    finally {
+        try {
+            if (set_1_1 && !set_1_1.done && (_a = set_1.return)) yield _a.call(set_1);
+        }
+        finally { if (e_1) throw e_1.error; }
+    }
+    //debugger
+    console.log(articles);
+    const articlesSet = new Set(articles.map(x => x.title));
+    const sorted = Array.from(articlesSet).sort();
+    console.log(sorted);
+    /* END getAllPagesInCat */
+    const pagesHaveEvolutionSection = getPagesHaveEvolutionSection(sorted);
+    console.log(pagesHaveEvolutionSection);
 }))();
-function getSubcatsByCatAndLevel(catName, level) {
+/* API */
+function getPagesHaveEvolutionSection(titles) {
+    var titles_1, titles_1_1;
+    var e_2, _a;
     return __awaiter(this, void 0, void 0, function* () {
-        console.log(`catName: ${catName}, level: ${level}`);
+        const pagesHaveEvolutionSection = [];
+        try {
+            for (titles_1 = __asyncValues(titles); titles_1_1 = yield titles_1.next(), !titles_1_1.done;) {
+                const title = titles_1_1.value;
+                const content = yield getContentOfPage(title);
+                // const content = (<any>contentObj).query.pages[0].revisions[0].slots.main.content;
+                debugger;
+                if ((content === null || content === void 0 ? void 0 : content.search("==Evo")) > -1) {
+                    pagesHaveEvolutionSection.push(title);
+                }
+            }
+        }
+        catch (e_2_1) { e_2 = { error: e_2_1 }; }
+        finally {
+            try {
+                if (titles_1_1 && !titles_1_1.done && (_a = titles_1.return)) yield _a.call(titles_1);
+            }
+            finally { if (e_2) throw e_2.error; }
+        }
+        return pagesHaveEvolutionSection;
+    });
+}
+function getSubcatsByCatAndLevel(catName, level) {
+    var e_3, _a;
+    return __awaiter(this, void 0, void 0, function* () {
         let subcats = yield getAllSubcats(catName);
         if (level > 0) {
             level--;
-            debugger;
-            for (let subcat of subcats) {
-                let results = yield getSubcatsByCatAndLevel(subcat.title, level);
-                subcats = [...subcats, ...results];
+            try {
+                for (var subcats_1 = __asyncValues(subcats), subcats_1_1; subcats_1_1 = yield subcats_1.next(), !subcats_1_1.done;) {
+                    let subcat = subcats_1_1.value;
+                    let results = yield getSubcatsByCatAndLevel(subcat.title, level);
+                    subcats = [...subcats, ...results];
+                }
+            }
+            catch (e_3_1) { e_3 = { error: e_3_1 }; }
+            finally {
+                try {
+                    if (subcats_1_1 && !subcats_1_1.done && (_a = subcats_1.return)) yield _a.call(subcats_1);
+                }
+                finally { if (e_3) throw e_3.error; }
             }
         }
         return subcats;
+    });
+}
+/* LOW LEVEL (using get) */
+function getContentOfPage(title) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const params = {
+            action: "query",
+            origin: "*",
+            rvprop: "content",
+            prop: "revisions",
+            formatversion: "2",
+            rvslots: "*",
+            format: "json",
+            titles: title,
+        };
+        return yield getContent(params);
+    });
+}
+function getAllPagesInCat(catName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const params = {
+            action: "query",
+            list: "categorymembers",
+            cmtitle: catName,
+            cmlimit: "500",
+            origin: "*",
+            cmtype: "page",
+            format: "json"
+        };
+        return yield getList(params);
     });
 }
 function getAllSubcats(catName) {
@@ -39,16 +139,16 @@ function getAllSubcats(catName) {
         const params = {
             action: "query",
             list: "categorymembers",
-            cmlimit: "500",
             cmtitle: catName,
+            cmlimit: "500",
             origin: "*",
             cmtype: "subcat",
             format: "json"
         };
-        return yield get(params);
+        return yield getList(params);
     });
 }
-function get(params) {
+function getList(params) {
     return __awaiter(this, void 0, void 0, function* () {
         const result = [];
         const queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
@@ -57,14 +157,29 @@ function get(params) {
         const json = yield resp.json();
         result.push(...json.query[params.list]);
         if (json.continue) {
+            debugger;
             const continueKey = Object.keys(json.continue).find(key => key != "continue");
             const newParams = Object.assign({}, params);
             newParams[continueKey] = json.continue[continueKey];
-            debugger;
-            const continueResult = yield get(newParams);
+            const continueResult = yield getList(newParams);
             result.push(...continueResult);
         }
         return result;
+    });
+}
+function getContent(params) {
+    var _a, _b, _c, _d, _e;
+    return __awaiter(this, void 0, void 0, function* () {
+        const result = [];
+        const queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
+        const url = BASE_URL + "?" + queryString;
+        const resp = yield fetch(url);
+        const json = yield resp.json();
+        if (!json.warnings) {
+            return (_e = (_d = (_c = (_b = (_a = json.query) === null || _a === void 0 ? void 0 : _a.pages[0]) === null || _b === void 0 ? void 0 : _b.revisions[0]) === null || _c === void 0 ? void 0 : _c.slots) === null || _d === void 0 ? void 0 : _d.main) === null || _e === void 0 ? void 0 : _e.content;
+        }
+        else
+            return null;
     });
 }
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -6,22 +6,53 @@ const BASE_URL = "https://en.wikipedia.org/w/api.php";
 
 let input = "Category:Animals";
 
+/* MAIN */
+
 (async () => {
-    const hi = await getSubcatsByCatAndLevel(input, 1);
-    debugger
+    /* START getSubcatsByCatAndLevel */
+    const hi = await getSubcatsByCatAndLevel(input, 0);
     console.log(hi);
     const set = new Set(hi.map(x => x.title));
     console.log(set);
+    /* END getSubcatsByCatAndLevel */
+
+    /* START getAllPagesInCat */
+    let articles = [];
+    //debugger
+    for await (const cat of set) {
+        articles.push(...await getAllPagesInCat(cat));
+    }
+    //debugger
+    console.log(articles);
+    const articlesSet = new Set(articles.map(x => x.title));
+    const sorted = Array.from(articlesSet).sort();
+    console.log(sorted);
+    /* END getAllPagesInCat */
+
+    const pagesHaveEvolutionSection = getPagesHaveEvolutionSection(sorted);
+    console.log(pagesHaveEvolutionSection);
+
 })();
 
-async function getSubcatsByCatAndLevel(catName, level) {
-    console.log(`catName: ${catName}, level: ${level}`);
+/* API */
+async function getPagesHaveEvolutionSection(titles) {
+    const pagesHaveEvolutionSection = [];
+    for await (const title of titles) {
+        const content = await getContentOfPage(title);
+        // const content = (<any>contentObj).query.pages[0].revisions[0].slots.main.content;
+        debugger
+        if (content?.search("==Evo") > -1) {
+            pagesHaveEvolutionSection.push(title)
+        }
+    }
+    return pagesHaveEvolutionSection;
+}
 
+async function getSubcatsByCatAndLevel(catName, level) {
     let subcats = await getAllSubcats(catName);
     if (level > 0) {
         level--;
-        debugger
-        for (let subcat of subcats) {
+        for await (let subcat of subcats) {
             let results = await getSubcatsByCatAndLevel(subcat.title, level);
             subcats = [...subcats, ...results];
         }
@@ -29,21 +60,52 @@ async function getSubcatsByCatAndLevel(catName, level) {
     return subcats;
 }
 
+/* LOW LEVEL (using get) */
+
+async function getContentOfPage(title) {
+    const params: IParams = <IRVParams>{
+        action: "query",
+        origin: "*",
+        rvprop: "content",
+        prop: "revisions",
+        formatversion: "2",
+        rvslots: "*",
+        format: "json",
+        titles: title,
+    };
+
+    return await getContent(params);
+}
+
+async function getAllPagesInCat(catName) {
+    const params: IParams = <ICMParams>{
+        action: "query",
+        list: "categorymembers",
+        cmtitle: catName,
+        cmlimit: "500",
+        origin: "*",
+        cmtype: "page",
+        format: "json"
+    };
+
+    return await getList(params);
+}
+
 async function getAllSubcats(catName) {
     const params: IParams = <ICMParams>{
         action: "query",
         list: "categorymembers",
-        cmlimit: "500",
         cmtitle: catName,
+        cmlimit: "500",
         origin: "*",
         cmtype: "subcat",
         format: "json"
     };
 
-    return await get(params);
+    return await getList(params);
 }
 
-async function get(params: IParams): Promise<any[]> {
+async function getList(params: IParams): Promise<any[]> {
     const result = [];
     const queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
     const url = BASE_URL + "?" + queryString;
@@ -53,16 +115,30 @@ async function get(params: IParams): Promise<any[]> {
     result.push(...json.query[params.list]);
 
     if (json.continue) {
+        debugger
         const continueKey = Object.keys(json.continue).find(key => key != "continue");
         const newParams = { ...params };
         newParams[continueKey] = json.continue[continueKey];
-        debugger
-        const continueResult: any[] = await get(newParams);
+        const continueResult: any[] = await getList(newParams);
         result.push(...continueResult);
     }
 
     return result;
 }
+
+async function getContent(params: IParams): Promise<string> {
+    const result = [];
+    const queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
+    const url = BASE_URL + "?" + queryString;
+    const resp = await fetch(url);
+    const json = await resp.json();
+
+    if (!json.warnings) {
+        return (<any>json).query?.pages[0]?.revisions[0]?.slots?.main?.content;
+    } else return null
+}
+
+/* MODELS */
 
 interface IParams {
     action: string;
@@ -76,4 +152,12 @@ interface ICMParams extends IParams {
     cmtype: "subcat" | "page" | "file";
     cmlimit: string;
     cmcontinue: string;
+}
+
+interface IRVParams extends IParams {
+    prop: string;
+    titles: string;
+    rvslots: string;
+    rvprop: string;
+    formatversion: string;
 }
