f31380a06b9b323cc67c890774a7058f567bc0d4
gilfriedman <gil.s.friedman@gmail.com>
2021-11-13 17:11:09 +0200

    from laptop

--- opensearch.js
+++ opensearch.js
@@ -2,12 +2,9 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const input = urlParams.get('input');
 
-// getSubcatsByCatAndLevel("Category:Animals", 2);
-
-fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=*&list=categorymembers&cmtitle=Category:${input}&format=json&cmlimit=500`)
+fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${input || prompt()}&limit=500`)
     .then(resp => resp.json())
     .then(async function (data) {
-        debugger
         document.write("<table><tr><td>name</td><td>url</td><td>he name</td></tr>");
 
         const urls = data[1].map(title => `https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=${title}&prop=langlinks&format=json&lllang=he&lllimit=500`);
@@ -26,28 +23,3 @@ fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=*&list=categorymem
                 + `</tr>`);
         }
     });
-
-async function getSubcatsByCatAndLevel(catName, level) {
-    // let subcats = [];
-    let resp = await fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=*&list=categorymembers&cmtitle=${catName}&cmtype=subcat&format=json&cmlimit=500`);
-    let json = await resp.json();
-    debugger
-    let subcats = json.query.categorymembers.map(cat => cat.title)
-    if (level > 0){
-        level--;
-        subcats.forEach(async subcat => {
-            let results = await getSubcatsByCatAndLevel(subcat, level);
-            subcats = [...subcats, ...results];
-        });
-    }
-    return subcats;
-}
-
-async function getAllSubcats(catName){
-    let resp = await fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=*&list=categorymembers&cmtitle=${catName}&cmtype=subcat&format=json&cmlimit=500`);
-    let json = await resp.json();
-    if (json.continue){
-
-    }
-    return 
-}
